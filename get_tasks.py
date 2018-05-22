d7a7581797e180c6af5df371af54544382ea598c
Jakob Prange <jp1724@georgetown.edu>
2018-05-21 20:03:40 -0400

    removed login credentials; added streusle2ucca.py

--- get_tasks.py
+++ get_tasks.py
@@ -10,8 +10,6 @@ import uccaapp.api as uapi
 
 
 server_address = "http://ucca.development.cs.huji.ac.il"
-username = "jp1724@georgetown.edu"
-password = "13e11i7l53e2"
 project = 92
 source = 16
 
@@ -20,10 +18,12 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--ids', type=str)
     parser.add_argument('-f', '--file', type=str)
+    parser.add_argument('-n', '--username', type=str)
+    parser.add_argument('-p', '--password', type=str)
     args = parser.parse_args()
     
     server_accessor = uapi.ServerAccessor(server_address=server_address, \
-            email=username, password=password, \
+            email=args.username, password=args.password, \
             auth_token="", project_id=project, source_id=source,\
                                           verbose=False)
 
